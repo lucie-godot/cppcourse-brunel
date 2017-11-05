55d0c9e19e85f01cd45ba25c3869e44249c48705
Lucie Godot <lucie.godot@epfl.ch>
2017-11-05 15:59:32 +0100

    add test

--- test.cpp
+++ test.cpp
@@ -27,10 +27,6 @@ TEST (NeuronTest, OneExcitatoryMbPotential) {
 	n2.n_update(9995, 0, 0);
 	
 	EXPECT_LT((20-n2.get_mb_potential()), pow(10, -8));
-	
-	
-	//Faire la même chose pour un nombre défini de pas de temps, vérifier les tenps de spikes.
-
 }
 
 
@@ -47,9 +43,7 @@ TEST (NeuronTest, OneInhibitory) {
 	n4.n_update(1000, 0, 0);
 	
 	EXPECT_LT (((20*(1-exp(-(0.1/20)))) - n1.get_mb_potential()), pow(10, -6));  //Test for one path of time
-	
 	EXPECT_LT ((0.3960265338 - n2.get_mb_potential()), pow(10, -6));
-	
 	EXPECT_LT ((0-n3.get_mb_potential()), pow(10, -8));
 	
 	n2.n_update(9995, 0, 0);
@@ -86,6 +80,15 @@ TEST (NeuronTest, NumberSpikesInhibitory) {
 	EXPECT_GT(n1.get_nb_spikes(), 1);
 	EXPECT_EQ(n2.get_nb_spikes(), 0);
 	EXPECT_EQ(n3.get_nb_spikes(), 0);
+}
+
+TEST (NeuronTest, ValueSpikes) {
+	Neuron n1 (0.1, 1.01, 0);
+	Neuron n2 (-0.5, 1.01, 0);
+	
+	n1.n_update(2000, 0, 0);
+	n2.n_update(10000, 0, 0);
+	
 }
 
 
